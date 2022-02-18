5b72b1dd07074e0ab78389404e57cf4462502e36
VIVEK0014 <98097208+VIVEK0014@users.noreply.github.com>
2022-02-18 20:52:54 +0530

    Add files via upload

---
+++ calc.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+//Defining class named calculator
+class calculator
+{
+	public:
+		int input1;
+		int input2;
+		
+		//Function for initializing the inputs
+		void setinput(int a,int b)
+		{
+			input1=a;
+			input2=b;
+		}
+		
+		//Function of class calculator type and returns the value of type calculator
+		calculator add(calculator obj1,calculator obj2)
+		{
+			calculator obj3; 
+			obj3.input1=obj1.input1+obj2.input1;
+			obj3.input2=obj1.input2+obj2.input2;
+			return obj3; 
+		}
+};
+
+int main()
+{
+	calculator obj1,obj2,obj3;
+	obj1.setinput(10,2);
+	obj2.setinput(5,5);
+	obj3=obj1.add(obj1,obj2);
+	cout<<"Result: "<<obj3.input1<<" "<<obj3.input2<<endl;
+	return 0;
+}
+
