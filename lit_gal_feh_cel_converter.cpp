ba47c6084e912ee22325f780d0fc743e1d6e364c
VIVEK0014 <98097208+VIVEK0014@users.noreply.github.com>
2022-03-10 12:46:22 +0530

    Add files via upload

---
+++ lit_gal_feh_cel_converter.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+class convert
+{
+    protected:
+    double val1;
+    double val2;
+    public:
+convert(double i)
+{
+    val1=i;
+}
+double getconv()
+{
+return val2;
+}
+
+double getinit()
+{
+return val1;
+}
+virtual void compute() = 0;
+};
+class l_to_g : public convert
+{
+    public:
+    l_to_g(double i) : convert(i){}
+    void compute()
+    {
+        val2=val1/3.7854;
+    }
+};
+
+class f_to_c : public convert
+{
+    public:
+    f_to_c(double i) : convert(i){}
+    void compute()
+    {
+        val2=(val1-32)/1.8;
+    }
+};
+
+int main ()
+{
+  convert *p;
+  l_to_g lgob(4);
+  f_to_c fcob(70);
+  p=&lgob;
+  cout<<"litres is : "<<p->getinit();
+  p->compute();
+  cout<<"\ngallons is : "<<p->getconv();
+  p=&fcob;
+  cout<<"\nfahrenhit is : "<<p->getinit();
+  p->compute();
+  cout<<"\ncelsius : "<<p->getconv();
+  return 0;
+}
